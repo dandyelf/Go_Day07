8014610664e8872be635f3900debfe3eadf5b580
Rus Sharafiev <srr@live.ru>
2024-08-12 15:49:37 +0300

    Add goroutines + fix test want value

--- ex00/coinsort/coinsort.go
+++ ex00/coinsort/coinsort.go
@@ -2,6 +2,8 @@ package coinsort
 
 import (
 	"slices"
+	"sort"
+	"sync"
 )
 
 func MinCoins(val int, coins []int) []int {
@@ -17,20 +19,33 @@ func MinCoins(val int, coins []int) []int {
 	return res
 }
 
+// Fixed
 func MinCoins2(val int, coins []int) []int {
-	return []int{2, 2, 2}
+	res := make([]int, 0)
+
+	if !slices.IsSorted(coins) {
+		slices.Sort(coins)
+	}
+	coins = slices.Compact(coins)
+
+	i := len(coins) - 1
+	for i >= 0 {
+		for val >= coins[i] {
+			val -= coins[i]
+			res = append(res, coins[i])
+		}
+		i -= 1
+	}
+	return res
 }
 
 // Optimized
 func MinCoins3(val int, coins []int) []int {
 	res := make([]int, 0)
 
-	// Sort coins
 	if !slices.IsSorted(coins) {
 		slices.Sort(coins)
 	}
-
-	// Remove duplicates
 	coins = slices.Compact(coins)
 
 	for i := len(coins) - 1; i >= 0; i-- {
@@ -46,4 +61,50 @@ func MinCoins3(val int, coins []int) []int {
 	}
 
 	return res
+}
+
+// Optimized with goroutines ------------------------------------------------------
+
+// Result struct
+type Result struct {
+	mu    sync.Mutex
+	wg    sync.WaitGroup
+	slice []int
+}
+
+// Add the method to appent coin
+func (r *Result) Add(coin int, qty int) {
+	r.mu.Lock()
+	for k := 1; k <= qty; k++ {
+		r.slice = append(r.slice, coin)
+		r.wg.Done()
+	}
+	r.mu.Unlock()
+}
+
+// Function
+func MinCoins4(val int, coins []int) []int {
+	res := Result{slice: make([]int, 0)}
+
+	if !slices.IsSorted(coins) {
+		slices.Sort(coins)
+	}
+	coins = slices.Compact(coins)
+
+	for i := len(coins) - 1; i >= 0; i-- {
+
+		if val >= coins[i] {
+			qty := val / coins[i]
+			val = val % coins[i]
+
+			res.wg.Add(qty)
+			go func() {
+				res.Add(coins[i], qty)
+			}()
+		}
+	}
+
+	res.wg.Wait()
+	sort.Sort(sort.Reverse(sort.IntSlice(res.slice)))
+	return res.slice
 }
--- ex00/coinsort/coinsort_test.go
+++ ex00/coinsort/coinsort_test.go
@@ -2,6 +2,7 @@ package coinsort_test
 
 import (
 	"leftrana/moneybag/ex00/coinsort"
+	"reflect"
 	"strconv"
 	"testing"
 )
@@ -22,17 +23,37 @@ func TestCreateImage(t *testing.T) {
 	}
 }
 
-func TestMinCoins3(t *testing.T) {
-	want := 1563412123342
-	coinDenominations := []int{1000, 20, 200, 100, 2, 5000, 10, 5, 1, 50}
-	res := coinsort.MinCoins3(want, coinDenominations)
+var amout = 99999
+var coinDenominations = []int{1000, 20, 200, 100, 2, 5000, 10, 5, 50, 1, 10000, 50000, 20000}
+var want = []int{50000, 20000, 20000, 5000, 1000, 1000, 1000, 1000, 200, 200, 200, 200, 100, 50, 20, 20, 5, 2, 2}
 
-	got := 0
-	for _, v := range res {
-		got += v
+func TestMinCoins(t *testing.T) {
+	got := coinsort.MinCoins(amout, coinDenominations)
+
+	if !reflect.DeepEqual(got, want) {
+		t.FailNow()
 	}
+}
 
-	if got != want {
-		t.Errorf("Got %v, want %v", got, want)
+func TestMinCoins2(t *testing.T) {
+	got := coinsort.MinCoins2(amout, coinDenominations)
+
+	if !reflect.DeepEqual(got, want) {
+		t.FailNow()
+	}
+}
+
+func TestMinCoins3(t *testing.T) {
+	got := coinsort.MinCoins3(amout, coinDenominations)
+
+	if !reflect.DeepEqual(got, want) {
+		t.FailNow()
+	}
+}
+func TestMinCoins4(t *testing.T) {
+	got := coinsort.MinCoins4(amout, coinDenominations)
+
+	if !reflect.DeepEqual(got, want) {
+		t.FailNow()
 	}
 }
