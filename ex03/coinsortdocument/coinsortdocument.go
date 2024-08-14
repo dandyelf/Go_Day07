9bc9434e0d7340a19858afd01848c76d5a8d9447
leftrana <dandyelf32@gmail.com>
2024-08-14 19:07:37 +0300

    coinsort doc add

---
+++ ex03/coinsortdocument/coinsortdocument.go
@@ -0,0 +1,89 @@
+package coinsortdocument
+
+import (
+	"math"
+	"slices"
+)
+
+// Original
+func MinCoins(val int, coins []int) []int {
+	res := make([]int, 0)
+	i := len(coins) - 1
+	for i >= 0 {
+		for val >= coins[i] {
+			val -= coins[i]
+			res = append(res, coins[i])
+		}
+		i -= 1
+	}
+	return res
+}
+
+// Fixed
+func MinCoins2(val int, coins []int) []int {
+	dp := make([]int, val+1)
+	usedCoins := make([]int, val+1)
+
+	for i := 1; i <= val; i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	dp[0] = 0
+
+	for _, denom := range coins {
+		for j := denom; j <= val; j++ {
+			if dp[j-denom]+1 < dp[j] {
+				dp[j] = dp[j-denom] + 1
+				usedCoins[j] = denom
+			}
+		}
+	}
+
+	if dp[val] == math.MaxInt32 {
+		return []int{}
+	}
+
+	result := []int{}
+	for val > 0 {
+		result = append(result, usedCoins[val])
+		val -= usedCoins[val]
+	}
+	return result
+}
+
+// Optimized
+func MinCoins2Optimized(val int, coins []int) []int {
+	if !slices.IsSorted(coins) {
+		slices.Sort(coins)
+	}
+	coins = slices.Compact(coins)
+	dp := make([]int, val+1)
+	usedCoins := make([]int, val+1)
+
+	for i := 1; i <= val; i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	dp[0] = 0
+
+	for _, denom := range coins {
+		for j := denom; j <= val; j++ {
+			if dp[j-denom]+1 < dp[j] {
+				dp[j] = dp[j-denom] + 1
+				usedCoins[j] = denom
+			}
+		}
+	}
+
+	if dp[val] == math.MaxInt32 {
+		return []int{}
+	}
+
+	result := []int{}
+	for val > 0 {
+		result = append(result, usedCoins[val])
+		val -= usedCoins[val]
+	}
+
+	return result
+}
