96c2af3e6da77450b5e0a2239d3cffcf39cb04e1
leftrana <dandyelf32@gmail.com>
2024-08-14 18:51:38 +0300

    top10 add

--- Makefile
+++ Makefile
@@ -6,6 +6,10 @@ tests: ex01 ex00
 ex00:
 	cd ./ex00/coinsort && go test -v .
 
-ex01:
-	cd ./ex01/coinsortbench && go test -benchmem -bench=.
+ex01: bench cputop
 	
+bench:
+	cd ./ex01/coinsortbench && go test -benchmem -bench=.
+
+cputop:
+	cd ./ex01 && go build . && ./ex01 && go tool pprof -top cpu.prof | head -n 17 > top10.txt
---
+++ ex01/coinsorttop10.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"leftrana/moneybag/ex01/coinsortbench"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+var amout int
+var coinDenominations []int
+
+func main() {
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		panic(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+	BenchmarkMinCoinsAmount999()
+	BenchmarkMinCoins2Amount999()
+	BenchmarkMinCoins2Optimized()
+	BenchmarkMinCoinsSl100()
+	BenchmarkMinCoins2Sl100()
+	BenchmarkMinCoins2OptimizedSl100()
+	BenchmarkMinCoinsN()
+	BenchmarkMinCoins2N()
+	BenchmarkMinCoins2OptimizedN()
+	BenchmarkMinCoins9999999()
+	BenchmarkMinCoins29999999()
+	BenchmarkMinCoins2Optimized9999999()
+	BenchmarkMinCoinsRepetitions()
+	BenchmarkMinCoins2Repetitions()
+	BenchmarkMinCoins2OptimizedRepetitions()
+	time.Sleep(10 * time.Second)
+}
+
+// Bench Lil test
+func BenchmarkMinCoinsAmount999() {
+	amout = 999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10000, 20000}
+	_ = coinsortbench.MinCoins(amout, coinDenominations)
+}
+
+func BenchmarkMinCoins2Amount999() {
+	amout = 999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10000, 20000}
+	_ = coinsortbench.MinCoins2(amout, coinDenominations)
+}
+
+func BenchmarkMinCoins2Optimized() {
+	amout = 999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10000, 20000}
+	_ = coinsortbench.MinCoins2Optimized(amout, coinDenominations)
+}
+
+// Bench with 100 slise and amount 99999
+func BenchmarkMinCoinsSl100() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	_ = coinsortbench.MinCoins(amout, coinDenominations)
+}
+
+func BenchmarkMinCoins2Sl100() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	_ = coinsortbench.MinCoins2(amout, coinDenominations)
+}
+
+func BenchmarkMinCoins2OptimizedSl100() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	_ = coinsortbench.MinCoins2Optimized(amout, coinDenominations)
+}
+
+// N repeating bench
+func BenchmarkMinCoinsN() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins(amout, coinDenominations)
+	}
+
+}
+
+func BenchmarkMinCoins2N() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins2(amout, coinDenominations)
+	}
+}
+
+func BenchmarkMinCoins2OptimizedN() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins2Optimized(amout, coinDenominations)
+	}
+}
+
+// Big Amount 999999999
+func BenchmarkMinCoins9999999() {
+	amout = 9999999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins(amout, coinDenominations)
+	}
+
+}
+
+func BenchmarkMinCoins29999999() {
+	amout = 9999999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins2(amout, coinDenominations)
+	}
+}
+
+func BenchmarkMinCoins2Optimized9999999() {
+	amout = 9999999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins2Optimized(amout, coinDenominations)
+	}
+}
+
+// coinDenominations duplicate bench
+func BenchmarkMinCoinsRepetitions() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 12, 13, 13, 13, 13, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 99, 99}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins(amout, coinDenominations)
+	}
+
+}
+
+func BenchmarkMinCoins2Repetitions() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 12, 13, 13, 13, 13, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 99, 99}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins2(amout, coinDenominations)
+	}
+}
+
+func BenchmarkMinCoins2OptimizedRepetitions() {
+	amout = 99999
+	coinDenominations = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 12, 13, 13, 13, 13, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 99, 99}
+	for i := 0; i < 5; i++ {
+		_ = coinsortbench.MinCoins2Optimized(amout, coinDenominations)
+	}
+}
